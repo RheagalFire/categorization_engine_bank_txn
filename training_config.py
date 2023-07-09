4c3d4c006b8ed22660bc82a2943a3e8afae8eb2b
RheagalFire <arishalam121@gmail.com>
2023-07-09 11:29:43 +0530

    Config file for Training

---
+++ training_config.py
@@ -0,0 +1,10 @@
+config = {
+    "svm_C": 0.1,
+    "gb_learning_rate": 0.1,
+    "gb_n_estimators": 100,
+    "gb_max_depth": 3,
+    "gb_min_samples_split": 2,
+    "random_search_n_iter": 10,
+    "random_search_cv": 5,
+    "random_search_random_state": 42
+}
